472757a3317c3e574f7bb11121d8db31af67c244
Szabo Vilmos <vili8899s@gmail.com>
2019-07-17 17:25:12 +0200

    v1.0.0

---
+++ util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyCWKf2FemUvgxn5LcFmTQ3Gq_exRvHnmic',
+  authDomain: 'chibi-screams.firebaseapp.com',
+  databaseURL: 'https://chibi-screams.firebaseio.com',
+  projectId: 'chibi-screams',
+  storageBucket: 'chibi-screams.appspot.com',
+  messagingSenderId: '610792737453',
+  appId: '1:610792737453:web:92943007fccfdd7d'
+};
